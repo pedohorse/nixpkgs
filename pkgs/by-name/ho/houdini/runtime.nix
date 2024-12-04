ff52f7071baf4bbf9f8196acf9f49bf7ef6439a1
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-12-04 16:43:25 +0100

    houdini: 20.5.370 -> 20.5.410

--- pkgs/by-name/ho/houdini/runtime.nix
+++ pkgs/by-name/ho/houdini/runtime.nix
@@ -1,11 +1,12 @@
 { requireFile, callPackage }:
 
 callPackage ./runtime-build.nix rec {
-  version = "20.5.370";
+  version = "20.5.410";
   eulaDate = "2021-10-13";
   src = requireFile {
     name = "houdini-${version}-linux_x86_64_gcc11.2.tar.gz";
-    hash = "sha256-QwPCU7E5yoJvWsiRUMBSAhEJYckbFTrQa1S4fto8dy0=";
+    hash = "sha256-8hvZFtLakSB15q7DKP9ZHvW53uEqIfBGxbiE2RItxDU=";
     url = "https://www.sidefx.com/download/daily-builds/?production=true";
   };
+  outputHash = "sha256-pS8LB7054WOHu0SPXjGbiz3ONXve2s+bwLmcBAdF/+c=";
 }
